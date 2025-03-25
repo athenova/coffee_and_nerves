f5b654056cb77563d7929bf5df4932fd156b6b17
athenova <aleksey.sergeyev@yandex.com>
2025-03-25 08:10:37 +0300

    Постер в ВК

--- project.py
+++ project.py
@@ -1,14 +1,20 @@
 from simple_blogger import Journalist
 from simple_blogger.generators.OpenAIGenerator import OpenAITextGenerator
+from simple_blogger.senders.TelegramSender import TelegramSender
+from simple_blogger.senders.VkSender import VkSender
 from datetime import datetime
 
 class Project(Journalist):
     def __init__(self, **kwargs):
         super().__init__(            
-            review_chat_id=-1002374309134,
             first_post_date=datetime(2025, 3, 6),
             text_generator=OpenAITextGenerator(),
             send_text_with_image=True,
+            reviewer=TelegramSender(),
+            senders=[
+                TelegramSender(channel_id='@coffee_and_nerves'),
+                VkSender(group_id='229838019')
+            ],
             topic_word_limit=100,
             **kwargs)
 
@@ -22,5 +28,3 @@ class Project(Journalist):
     
     def _system_prompt(self, _):
         return f'Ты - руководитель HR, лидер команды со 100% харизмой, всегда оптимистично настроенный и с отличным чувством юмора'
-   
-   
